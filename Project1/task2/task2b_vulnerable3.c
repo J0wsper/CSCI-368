181ee4a90b46d57248398c0a384bd15aa000abe7
J0wsper <bramschuijff03@gmail.com>
2025-02-26 09:06:06 -0800

    More polished version of the input filter

--- Project1/task2/task2b_vulnerable3.c
+++ Project1/task2/task2b_vulnerable3.c
@@ -16,51 +16,41 @@ static char response[512];
 static char input_buffer[512];
 static sender_info sender;
 
-int allowlist_finder(char* input, char* allowlist_line) {
-    for (int i = 0; input[i] != '\0' && i < 128; i++) {
-        int pass = 0;
-        for (int j = 0; allowlist_line[j] != '\0'; j++) {
-            if (input[i] == allowlist_line[j]) {
-                pass = 1;
-                break;
-            }
+int remove_percent(char* input, const size_t length) {
+    char* src = input;
+    char* dest = input;
+    
+    // Kind of hacky but it works
+    for (int i = 0; *src != '\0' && i < length; src++) {
+        *dest = *src;
+        if (*dest != '%') {
+            dest++;
         }
-        if (!pass) {
-            return 0;
-        } 
     }
-    return 1;
+
+    // Add the null terminator
+    *dest = '\0';
+    input = dest;
+    return 0;
 }
 
 char* your_input_filter (char *input)
 {
-    FILE *config_file;
-    config_file = fopen("allowlist", "r");
-    char f_buffer[128];
-
-    if (config_file == NULL) {
-        perror("Error opening the configuration file.");
-        return EXIT_SUCCESS;
-    }
-    while (fgets(f_buffer, sizeof(f_buffer), config_file) != NULL) { 
-        // read the allowed input from the config file, line by line
-        if (f_buffer[strlen(f_buffer) - 1] == '\n') {
-            f_buffer[strlen(f_buffer) - 1] = '\0';
-        }
-        int pass = allowlist_finder(input, f_buffer);
-        if (!pass) {
-            perror("Input did not pass the allowlist");
-            return EXIT_SUCCESS;
-        }
-    }
+    //NOTE: This solution doesn't use allowlist. I opted instead to just remove percent signs
+    //indiscriminately because they could be format specifiers and this is the main type of 
+    //behavior we are trying to avoid here.
     const size_t input_size = 64;
     const size_t input_len = strnlen(input, input_size);
+
+    // Copying over our input into its bounds.
     memcpy(input, input, input_len);
-
-    // NOTE: This will always cause a segmentation faults in some cases. However, in the use case present
-    // in vulnerable3.c, it should be fine.
-    memset(input+input_len, '\0', 1);
-
+    int res = remove_percent(input, input_len);
+    
+    // If somehow res is not 0, return that something bad happened
+    if (!res) {
+       perror("Sanitization unsuccessful, stopping now.");
+       return NULL;
+    }
     return input;
 }
 
