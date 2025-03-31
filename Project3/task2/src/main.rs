f2f2a83c85d900f80248a523bd01f56c27146120
J0wsper <bramschuijff03@gmail.com>
2025-03-31 14:05:45 -0700

    Reimplemented how the file performs the xxd dump

--- Project3/task2/src/main.rs
+++ Project3/task2/src/main.rs
@@ -1,17 +1,25 @@
 use std::{
     env,
     fs::{self, File},
-    io::{self, BufRead, BufReader, Read},
+    io::{self, BufRead, BufReader, BufWriter, Read, Write},
 };
 
 fn main() {
-
     // Getting our file and turning it into a hex dump
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    let raw_contents = fs::read_to_string(file_path).expect("Could not find input file");
-    let hex_contents = xxd::convert::bytes(&raw_contents);
-    let hex_file = File::create_new("dump")
+    let mut buf: Vec<u8> = Vec::new();
+    let _ = File::open(file_path)
+        .expect("Could not find file")
+        .read_to_end(&mut buf)
+        .expect("Could not read file");
+    let hex_file = match File::create_new("xxd_dump.txt") {
+        Ok(contents) => contents,
+        Err(..) => File::create("xxd_dump.txt").unwrap(),
+    };
+    let mut writer = BufWriter::new(&hex_file);
+    let _ = hxdmp::hexdump(&buf, &mut writer);
 
-    println!("Hello, world!");
+    // Reading the xxd dump and seeing what we can find
+    let reader = BufReader::new(&hex_file);
 }
