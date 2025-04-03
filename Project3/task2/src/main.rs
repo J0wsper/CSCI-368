277fc8b27bbe56bca211bbc5bc4ee2e4d1206b16
J0wsper <bramschuijff03@gmail.com>
2025-04-03 11:24:40 -0700

    ??????? Trying out some cursed data structures

--- Project3/task2/src/main.rs
+++ Project3/task2/src/main.rs
@@ -1,28 +1,73 @@
-use std::{env, fs::File, io::Read};
+use std::{collections::BTreeMap, env, fs::File, io::Read};
 
 const BLOCK_SIZE: usize = 16;
 
-// Enum to carry the request header types and their associated locations
+// Struct to carry information about which headers we have found
 #[derive(Debug)]
-enum ReqHeader {
-    Balance(usize),
-    Invoice(usize),
-    Transfer(usize),
+struct Headers {
+    balance: Option<usize>,
+    invoice: Option<usize>,
+    transfer: Option<usize>,
 }
 
-#[derive(Debug)]
-struct IsReqType {
-    balance: bool,
-    invoice: bool,
-    transfer: bool,
-}
-
-impl IsReqType {
+impl Headers {
     pub fn new() -> Self {
         Self {
-            balance: true,
-            invoice: true,
-            transfer: true,
+            balance: None,
+            invoice: None,
+            transfer: None,
+        }
+    }
+    pub fn found_balance(&mut self, loc: usize) {
+        self.balance = Some(loc);
+    }
+    pub fn found_invoice(&mut self, loc: usize) {
+        self.invoice = Some(loc)
+    }
+    pub fn found_transfer(&mut self, loc: usize) {
+        self.transfer = Some(loc)
+    }
+    pub fn num_found(&self) -> u8 {
+        let mut found = 0;
+        if self.balance.is_some() {
+            found += 1;
+        }
+        if self.transfer.is_some() {
+            found += 1;
+        }
+        if self.invoice.is_some() {
+            found += 1;
+        }
+        found
+    }
+}
+
+struct Accounts<'a>(BTreeMap<&'a [u8], Vec<&'a [u8]>>);
+
+impl<'a> Accounts<'a> {
+    pub fn new(buf: &'a [u8]) -> Self {
+        Self(find_accs(buf))
+    }
+    pub fn get(&self, buf: &[u8], idx: usize) -> Option<&Vec<&[u8]>> {
+        self.0.get(&buf[idx..idx + BLOCK_SIZE])
+    }
+}
+
+// Holds the state that we're in when we're recursing
+struct State<'a> {
+    buf: &'a [u8],
+    chunks: Vec<&'a [u8]>,
+    accs: Accounts<'a>,
+    headers: Headers,
+}
+
+impl<'a> State<'a> {
+    pub fn new(buf: &'a [u8]) -> Self {
+        Self {
+            buf,
+            chunks: vec![buf],
+            accs: Accounts::new(buf),
+            headers: Headers::new(),
         }
     }
 }
@@ -41,28 +86,6 @@ fn create_buf() -> Vec<u8> {
         .read_to_end(&mut buf)
         .expect("Could not read file");
     buf
-}
-
-// Utility function to turn block numbers into (zero-indexed) line numbers
-fn block_to_line(blocks: &[usize]) -> Vec<usize> {
-    blocks.iter().map(|x| x / BLOCK_SIZE).collect()
-}
-
-// // Utility function to find the number of transfer requests based on the fact that 5 is prime
-// fn num_transfers(buf: &[u8]) -> u32 {
-//     let mut len = buf.len();
-//     while len % 2 == 0 {
-//         len /= 2;
-//     }
-//     (len / 5).try_into().unwrap()
-// }
-
-// Finds the minimum difference between any two consecutive elements in a vector
-fn min_diff(vec: &[usize]) -> usize {
-    vec.windows(2)
-        .map(|x| x[0].abs_diff(x[1]))
-        .min_by(|x, y| x.partial_cmp(y).unwrap())
-        .unwrap()
 }
 
 // Finds all instances of the block that starts at start in buf
@@ -98,93 +121,38 @@ fn find_block(buf: &[u8], start: usize) -> Vec<usize> {
 
 // We know that if our initial request header is followed by two different blocks at different
 // points in our buffer, then both of those blocks are account labels.
-// NOTE: There is no guarantee that this will find all of the account numbers. It does a best
-// effort search based on the fact that we know the first block is a request header.
-fn find_accs(buf: &[u8]) -> Vec<usize> {
-    // Both of these vectors are implicitly sorted based on how find_block operates.
+fn find_accs(buf: &[u8]) -> BTreeMap<&[u8], Vec<&[u8]>> {
     let first_req = find_block(buf, 0);
-    let first_acc = find_block(buf, BLOCK_SIZE);
-    let mut accs = first_acc.clone();
-    // For each instance of the first request header, check which blocks follow it
+    let first_acc = &buf[BLOCK_SIZE..2 * BLOCK_SIZE];
+    let first_accs = find_block(buf, BLOCK_SIZE);
+    let mut accs = BTreeMap::new();
+    let mut first_val = accs
+        .insert(first_acc, vec![&buf[BLOCK_SIZE..2 * BLOCK_SIZE]])
+        .unwrap();
+    for start_idx in first_accs {
+        first_val.push(&buf[start_idx..start_idx + BLOCK_SIZE]);
+    }
     for block in first_req.iter() {
-        let following = block + BLOCK_SIZE;
-        match accs.binary_search(&following) {
-            // If we found the following block in our first account vector, continue
-            Ok(_) => continue,
-            // Otherwise, we want to find all instances of it
-            Err(_) => {
-                let new_acc = find_block(buf, following);
-                accs = [accs, new_acc].concat();
-                accs.sort();
+        let following = &buf[*block..*block + BLOCK_SIZE];
+        match accs.get_mut(following) {
+            Some(header) => header.push(following),
+            None => {
+                accs.insert(following, vec![following]).unwrap();
             }
         }
     }
     accs
 }
 
-// Trys all of the given headers to see if they're valid
-fn find_reqs(buf: &[u8], accs: &[usize], init: &[usize]) -> ReqHeader {
-    // Finding how many of the initial requests we have
-    let init_num = init.len();
-
-    // Bookkeeping the types of request headers we've tried for the initial variable
-    let mut is_init = IsReqType::new();
-
-    // Finding the smallest difference between any two consecutive initial requests
-    let min_diff = min_diff(init);
-    // dbg!(init);
-    println!("Min diff: {}", min_diff);
-
-    // Performing all of our initial ruling out
-
-    // If the fifth block is an account number or if the minimum difference between any two
-    // consecutive initial requests is less than 5, the initial request cannot be a transfer.
-    if accs.binary_search(&(5 * BLOCK_SIZE)).is_ok() || min_diff < 5 * BLOCK_SIZE {
-        is_init.transfer = false;
-    }
-
-    // If the second block is an account number or the minimum difference is 5, then the initial
-    // request cannot be a balance request
-    if accs.binary_search(&(2 * BLOCK_SIZE)).is_ok() || min_diff == 5 * BLOCK_SIZE {
-        is_init.balance = false;
-    }
-
-    // If the minimum difference is less than 4 or equal to 5, then the initial request cannot be
-    // an invoice request.
-    if min_diff < 4 * BLOCK_SIZE || min_diff == 5 * BLOCK_SIZE {
-        is_init.invoice = false;
-    }
-
-    // Error handling
-    if (is_init.transfer && is_init.balance)
-        || (is_init.transfer && is_init.invoice)
-        || (is_init.balance && is_init.invoice)
-    {
-        panic!("Initial request type not well-defined");
-    } else if !is_init.transfer && !is_init.balance && !is_init.invoice {
-        panic!("Invalid request sequence detected, could not derive initial request type");
-    }
-
-    // If it passed the error handling, then we know the type of our initial request header
-    if is_init.transfer {
-        ReqHeader::Transfer(0)
-    } else if is_init.invoice {
-        ReqHeader::Invoice(0)
-    } else {
-        ReqHeader::Balance(0)
-    }
+fn solve(state: State) -> State {
+    // Base case: we check if there are any contradictions
+    if state.headers.num_found() == 3 {}
+    todo!()
 }
 
 fn main() {
     // Getting our file
     let buf = create_buf();
-    println!("Total lines: {}", buf.len() / BLOCK_SIZE);
-    // Finding (some of) our account numbers
-    let accs = find_accs(&buf);
-    // let acc_lines = block_to_line(&accs);
-    // Finding our initial request headers
-    let init = find_block(&buf, 0);
-    // let init_lines = block_to_line(&init);
-    let init_type = find_reqs(&buf, &accs, &init);
-    dbg!(init_type);
+    let state = State::new(&buf);
+    solve(state);
 }
