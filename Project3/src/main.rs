41e0cf2f49ac36c84d7a2581306ed026ba078416
J0wsper <bramschuijff03@gmail.com>
2025-04-04 15:54:25 -0700

    Fixed some bugs with is_clear and initial_screen

--- Project3/src/main.rs
+++ Project3/src/main.rs
@@ -13,6 +13,11 @@ enum ReqType {
     Transfer,
 }
 
+enum ClearType {
+    Full,
+    Partial(ReqType),
+}
+
 #[derive(Debug, Clone)]
 struct Headers<'a> {
     balance: Option<&'a [u8]>,
@@ -44,36 +49,56 @@ impl<'a> State<'a> {
             },
         }
     }
+    fn is_solved(&self) -> bool {
+        self.headers.transfer.is_some()
+            && self.headers.invoice.is_some()
+            && self.headers.balance.is_some()
+    }
     pub fn print_pt2(&self) {
-        if self.headers.transfer.is_none()
-            || self.headers.invoice.is_none()
-            || self.headers.balance.is_none()
-        {
+        if !self.is_solved() {
             println!(
                 "State not properly found! Attempt to initialize first with the solve function"
             );
-        } else {
-            for i in 0..3 {
-                match i {
-                    0 => {
-                        let idx = find_header(self.buf, self.headers.balance.unwrap());
-                        let occurences = find_block(self.buf, idx).len();
-                        println!("Balance occurences: {}", occurences);
-                    }
-                    1 => {
-                        let idx = find_header(self.buf, self.headers.invoice.unwrap());
-                        let occurences = find_block(self.buf, idx).len();
-                        println!("Invoice occurences: {}", occurences);
-                    }
-                    2 => {
-                        let idx = find_header(self.buf, self.headers.transfer.unwrap());
-                        let occurences = find_block(self.buf, idx).len();
-                        println!("Transfer occurences: {}", occurences);
-                    }
-                    _ => {
-                        panic!("Out of bound req type when printing");
-                    }
+            return;
+        }
+        for i in 0..3 {
+            match i {
+                0 => {
+                    let idx = find_header(self.buf, self.headers.balance.unwrap());
+                    let occurences = find_block(self.buf, idx).len();
+                    println!("Balance occurences: {}", occurences);
                 }
+                1 => {
+                    let idx = find_header(self.buf, self.headers.invoice.unwrap());
+                    let occurences = find_block(self.buf, idx).len();
+                    println!("Invoice occurences: {}", occurences);
+                }
+                2 => {
+                    let idx = find_header(self.buf, self.headers.transfer.unwrap());
+                    let occurences = find_block(self.buf, idx).len();
+                    println!("Transfer occurences: {}", occurences);
+                }
+                _ => {
+                    panic!("Out of bound req type when printing");
+                }
+            }
+        }
+    }
+    pub fn print_pt3(&self) {
+        if !self.is_solved() {
+            println!(
+                "State not properly found! Attempt to initialize first with the solve function"
+            );
+            return;
+        }
+        for (i, chunk) in self.chunks.iter().enumerate() {
+            let header = &chunk[0..BLOCK_SIZE];
+            if header == self.headers.balance.unwrap() {
+                println!("Request {} is a balance request", i);
+            } else if header == self.headers.invoice.unwrap() {
+                println!("Request {} is an invoice request", i);
+            } else {
+                println!("Request {} is a transfer request", i);
             }
         }
     }
@@ -136,7 +161,7 @@ fn find_accs<'a>(buf: &'a [u8], header: &'a [u8]) -> HashSet<&'a [u8]> {
     let loc = find_header(buf, header);
     let instances = find_block(buf, loc);
     for req in instances.iter() {
-        let acc = &buf[*req..*req + BLOCK_SIZE];
+        let acc = &buf[*req + BLOCK_SIZE..*req + 2 * BLOCK_SIZE];
         accs.insert(acc);
     }
     accs
@@ -172,49 +197,47 @@ fn compose_state<'a>(
     state.chunks.remove(idx);
     state.chunks.push(new_chunks.0);
     state.chunks.push(new_chunks.1);
-    state.chunks.sort();
-    match req_type {
-        ReqType::Balance => {
-            let new_accs = find_accs(state.buf, &chunk[BLOCK_SIZE..2 * BLOCK_SIZE]);
-            state.accs.extend(&new_accs);
-        }
-        _ => {
-            let new_accs1 = find_accs(state.buf, &chunk[BLOCK_SIZE..2 * BLOCK_SIZE]);
-            let new_accs2 = find_accs(state.buf, &chunk[2 * BLOCK_SIZE..3 * BLOCK_SIZE]);
-            state.accs.extend(&new_accs1);
-            state.accs.extend(&new_accs2);
-        }
-    }
+    let new_accs = find_accs(state.buf, &chunk[0..BLOCK_SIZE]);
+    state.accs.extend(&new_accs);
     state
 }
 
 fn initial_screen(
-    header: &[u8],
     chunk: &[u8],
     accs: &HashSet<&[u8]>,
-    taken_headers: &Headers,
+    headers: &Headers,
     req_type: ReqType,
 ) -> bool {
     let chunk_len = chunk.len();
-    let div = match req_type {
-        ReqType::Balance => BALANCE_SIZE,
-        ReqType::Invoice => INVOICE_SIZE,
-        ReqType::Transfer => TRANSFER_SIZE,
+    let (div, header_holder) = match req_type {
+        ReqType::Balance => (BALANCE_SIZE, headers.balance),
+        ReqType::Invoice => (INVOICE_SIZE, headers.invoice),
+        ReqType::Transfer => (TRANSFER_SIZE, headers.transfer),
     };
-    let header_holder = match req_type {
-        ReqType::Balance => taken_headers.balance,
-        ReqType::Invoice => taken_headers.invoice,
-        ReqType::Transfer => taken_headers.transfer,
-    };
-    (chunk_len > div && !accs.contains(&chunk[div..div + BLOCK_SIZE]) && header_holder.is_none())
-        || header_holder == Some(header)
+    chunk_len > div && !accs.contains(&chunk[div..div + BLOCK_SIZE]) && header_holder.is_none()
 }
 
-fn is_req_header(buf: &[u8], headers: &Headers) -> bool {
-    if buf.len() != BLOCK_SIZE {
+// Clearing a chunk as valid
+fn is_clear(chunk: &[u8], headers: &Headers) -> Option<ClearType> {
+    let header = &chunk[0..BLOCK_SIZE];
+    let len = chunk.len();
+    if len % BLOCK_SIZE != 0 {
         panic!("Invalid buffer length when attempting to parse header");
     }
-    Some(buf) == headers.balance || Some(buf) == headers.invoice || Some(buf) == headers.transfer
+    if (Some(header) == headers.balance && len == BALANCE_SIZE)
+        || (Some(header) == headers.invoice && len == INVOICE_SIZE)
+        || (Some(header) == headers.transfer && len == TRANSFER_SIZE)
+    {
+        Some(ClearType::Full)
+    } else if Some(header) == headers.balance && len > BALANCE_SIZE {
+        Some(ClearType::Partial(ReqType::Balance))
+    } else if Some(header) == headers.invoice && len > INVOICE_SIZE {
+        Some(ClearType::Partial(ReqType::Invoice))
+    } else if Some(header) == headers.transfer && len > TRANSFER_SIZE {
+        Some(ClearType::Partial(ReqType::Transfer))
+    } else {
+        None
+    }
 }
 
 // We assume that our state.headers are all valid for a single iteration of the solve algorithm.
@@ -223,51 +246,59 @@ fn is_req_header(buf: &[u8], headers: &Headers) -> bool {
 fn solve(state: State) -> Option<State> {
     // Step 1: find an unsolved chunk
     for (i, chunk) in state.chunks.iter().enumerate() {
-        let len = chunk.len();
         let header = &chunk[0..BLOCK_SIZE];
-        // If our chunk length is invalid
-        if is_req_header(header, &state.headers) {
-            continue;
-        }
-        if len != BALANCE_SIZE && len != INVOICE_SIZE && len != TRANSFER_SIZE {
-            for j in 0..3 {
-                let req_type = match j {
-                    0 => ReqType::Transfer,
-                    1 => ReqType::Invoice,
-                    2 => ReqType::Balance,
-                    _ => panic!("Out of bound req type attempt"),
-                };
-                // If our assignment does not pass the initial screening, then we simply continue
-                // and try a different assignment
-                if !initial_screen(header, chunk, &state.accs, &state.headers, req_type) {
-                    continue;
-                }
-                // Otherwise, we try out that configuration and create a new state object
-                let new_state = state.clone();
-                let mut new_state = compose_state(new_state, chunk, i, req_type);
-                match j {
-                    0 => {
-                        new_state.headers.transfer = Some(header);
+        if let Some(clear_type) = is_clear(chunk, &state.headers) {
+            match clear_type {
+                // If we have a fully-cleared chunk, continue
+                ClearType::Full => continue,
+                // If we have a partially-cleared chunk, we split it
+                ClearType::Partial(req_type) => {
+                    let new_state = compose_state(state.clone(), chunk, i, req_type);
+                    let ret = solve(new_state);
+                    if let Some(cand) = ret {
+                        if validate_chunks(&cand.chunks, &cand.accs) {
+                            return Some(cand);
+                        }
                     }
-                    1 => {
-                        new_state.headers.invoice = Some(header);
-                    }
-                    2 => {
-                        new_state.headers.balance = Some(header);
-                    }
-                    _ => panic!("Out of bound req attempt"),
-                };
-                let ret = solve(new_state);
-                if let Some(cand) = ret {
-                    if validate_chunks(&cand.chunks, &cand.accs) {
-                        return Some(cand);
-                    }
+                    return None;
                 }
             }
-            return None;
-        } else {
-            continue;
         }
+        for j in 0..3 {
+            let req_type = match j {
+                0 => ReqType::Transfer,
+                1 => ReqType::Invoice,
+                2 => ReqType::Balance,
+                _ => panic!("Out of bound req type attempt"),
+            };
+            // If our assignment does not pass the initial screening, then we simply continue
+            // and try a different assignment
+            if !initial_screen(chunk, &state.accs, &state.headers, req_type) {
+                continue;
+            }
+            // Otherwise, we try out that configuration and create a new state object
+            let new_state = state.clone();
+            let mut new_state = compose_state(new_state, chunk, i, req_type);
+            match j {
+                0 => {
+                    new_state.headers.transfer = Some(header);
+                }
+                1 => {
+                    new_state.headers.invoice = Some(header);
+                }
+                2 => {
+                    new_state.headers.balance = Some(header);
+                }
+                _ => panic!("Out of bound req attempt"),
+            };
+            let ret = solve(new_state);
+            if let Some(cand) = ret {
+                if validate_chunks(&cand.chunks, &cand.accs) {
+                    return Some(cand);
+                }
+            }
+        }
+        return None;
     }
     Some(state)
 }
@@ -277,5 +308,9 @@ fn main() {
     // Getting our file
     let buf = create_buf();
     let state = State::new(&buf);
-    let _ret = solve(state);
+    let ret = solve(state);
+    match ret {
+        Some(i) => i.print_pt3(),
+        None => println!("No valid assignment found"),
+    }
 }
